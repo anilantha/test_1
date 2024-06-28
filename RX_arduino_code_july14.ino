7ea88a318b9bd055c79ebeef9debd7833779b6e1
anilantha <86286249+anilantha@users.noreply.github.com>
2024-06-28 15:49:13 +0300

    Update RX_arduino_code_july14.ino

--- RX_arduino_code_july14.ino
+++ RX_arduino_code_july14.ino
@@ -1,10 +1,4 @@
-/*
- * IRremote: IRrecvDemo - demonstrates receiving IR codes with IRrecv
- * An IR detector/demodulator must be connected to the input RECV_PIN.
- * Version 0.1 July, 2009
- * Copyright 2009 Ken Shirriff
- * http://arcfn.com
- */
+
 
 #include <IRremote.h>
 
@@ -17,8 +11,7 @@ decode_results results;
 void setup()
 {
   Serial.begin(9600);
-  // In case the interrupt driver crashes on setup, give a clue
-  // to the user what's going on.
+
 //  Serial.println("Enabling IRin");
   irrecv.enableIRIn(); // Start the receiver
 //  Serial.println("Enabled IRin");
@@ -53,33 +46,3 @@ Serial.println(extractedNodeID);
 
   
 }
-
-//#include <IRremote.h>
-//
-//const int RECEIVE_PIN = 11;  // Pin connected to the IR receiver
-//const int DATA_BITS = 8;     // Number of bits in the received payload
-//
-//IRrecv irrecv(RECEIVE_PIN);
-//decode_results results;
-//
-//void setup() {
-//  Serial.begin(9600);
-//  irrecv.enableIRIn();
-//}
-//
-//void loop() {
-//  if (irrecv.decode(&results)) {
-//    if (results.bits == DATA_BITS) {
-//      // Extract the temperature and node number from the received payload
-//      int receivedTemp = results.value / 10;  // Extract the temperature (two digits)
-//      int receivedNode = results.value % 10;  // Extract the node number (one digit)
-//
-//      // Print the received values
-//      Serial.print("Received Temperature: ");
-//      Serial.println(receivedTemp);
-//      Serial.print("Received Node: ");
-//      Serial.println(receivedNode);
-//    }
-//    irrecv.resume();
-//  }
-//}
